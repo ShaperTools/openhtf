c90ba3470afc370f413c41ef272a9e910130916c
Jeremy Blum <sciguy14@users.noreply.github.com>
2017-10-05 09:09:22 -0700

    Cleaned up the reporting of when test phases execute.

--- openhtf/core/phase_executor.py
+++ openhtf/core/phase_executor.py
@@ -225,12 +225,12 @@ class PhaseExecutor(object):
     """Executes the given phase, returning a PhaseExecutionOutcome."""
     # Check this before we create a PhaseState and PhaseRecord.
     if phase_desc.options.run_if and not phase_desc.options.run_if():
-      _LOG.info('Phase %s skipped due to run_if returning falsey.',
+      _LOG.info('Phase "%s" skipped due to run_if returning falsey.',
                 phase_desc.name)
       return PhaseExecutionOutcome(openhtf.PhaseResult.SKIP)
 
     with self.test_state.running_phase_context(phase_desc) as phase_state:
-      _LOG.info('Executing phase %s', phase_desc.name)
+      _LOG.info('>>> Now executing phase: "%s" <<<', phase_desc.name)
       phase_thread = PhaseExecutorThread(phase_desc, self.test_state)
       phase_thread.start()
       self._current_phase_thread = phase_thread
@@ -240,7 +240,7 @@ class PhaseExecutor(object):
         phase_state.hit_repeat_limit = True
         result = PhaseExecutionOutcome(openhtf.PhaseResult.STOP)
 
-    _LOG.debug('Phase finished with result %s', result)
+    _LOG.debug('Phase finished with result: "%s"', result)
     return result
 
   def stop(self, timeout_s=None):
